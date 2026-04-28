6a6be0ef7459678683d403d8986b334352b1980b
mordocconicole <nicole.mordocco@mail.polimi.it>
2026-04-28 18:46:08 +0200

    configure typography tokens

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import favicon from '$lib/assets/favicon.svg';
 
 	import "$lib/styles/reset.css";
+	import "$lib/styles/tokens.css";
 
 	let { children } = $props();
 </script>
@@ -15,9 +16,9 @@
 	<link href="https://api.fontshare.com/v2/css?f[]=satoshi@1,2&display=swap" rel="stylesheet">
 
 	<!-- Inter Font -->
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.bunny.net">
+	<link rel="preconnect" href="https://fonts.bunny.net" crossorigin>
+	<link href="https://fonts.bunny.net/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
 </svelte:head>
 
 {@render children()}
