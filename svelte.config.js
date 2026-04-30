3dca674b7e2a38f0dd7dd295bf73cee36e7e54c9
mordocconicole <nicole.mordocco@mail.polimi.it>
2026-04-30 15:32:52 +0200

    fix deploy

--- svelte.config.js
+++ svelte.config.js
@@ -4,22 +4,20 @@ import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', '.md'],
-	preprocess: [vitePreprocess(), mdsvex({ extensions: ['.md']})],
-	compilerOptions: {
-		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
-	},
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true})
-	}
+    extensions: ['.svelte', '.md'],
+    preprocess: [vitePreprocess(), mdsvex({ extensions: ['.md']})],
+    compilerOptions: {
+        runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
+    },
+    kit: {
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: '404.html', // Modificato: permette a Netlify di gestire le rotte
+            precompress: false,
+            strict: true
+        })
+    }
 };
 
 export default config;
